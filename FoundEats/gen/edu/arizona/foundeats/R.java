8b75871bf3d10c3a29498fe0be22a59de4cd1ddf
Kristoffer Cabulong <kcabulon@email.arizona.edu>
2015-04-02 16:19:38 -0700

    Added a bunch of empty classes to build on

--- FoundEats/gen/edu/arizona/foundeats/R.java
+++ FoundEats/gen/edu/arizona/foundeats/R.java
@@ -28,15 +28,42 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_achievements=0x7f030000;
+        public static final int activity_challenge=0x7f030001;
+        public static final int activity_food=0x7f030002;
+        public static final int activity_hot_to_play=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_make_meal=0x7f030005;
+        public static final int activity_map=0x7f030006;
+        public static final int activity_pick_old_meal=0x7f030007;
+        public static final int activity_score=0x7f030008;
+        public static final int activity_type_of_meal=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int achievements=0x7f070000;
+        public static final int challenge=0x7f070001;
+        public static final int food=0x7f070002;
+        public static final int hot_to_play=0x7f070003;
+        public static final int main=0x7f070004;
+        public static final int make_meal=0x7f070005;
+        public static final int map=0x7f070006;
+        public static final int pick_old_meal=0x7f070007;
+        public static final int score=0x7f070008;
+        public static final int type_of_meal=0x7f070009;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_achievements=0x7f050008;
+        public static final int title_activity_challenge=0x7f050003;
+        public static final int title_activity_food=0x7f050009;
+        public static final int title_activity_hot_to_play=0x7f050006;
+        public static final int title_activity_make_meal=0x7f050007;
+        public static final int title_activity_map=0x7f050004;
+        public static final int title_activity_pick_old_meal=0x7f05000b;
+        public static final int title_activity_score=0x7f05000a;
+        public static final int title_activity_type_of_meal=0x7f050005;
     }
     public static final class style {
         /** 
